2688accef172fdeb4f4e7feb04b80f44ed95980d
Rosemary Beetle <rosemarybeetle@gmail.com>
2014-02-23 20:10:13 +0000

    split tweets into constituent parts

--- processTweets.py
+++ processTweets.py
@@ -9,11 +9,43 @@ def processTweets ():
     contents = open('tweetstore2.json', 'r')
     cons = contents.read()
     ct = json.loads(cons)
-    print (ct['store'])
+    #print(max)
+    global maxy
     x=0
-    
-    vv=ct['store'][5]['username']
-    print(str(vv))
-    print 
+    while (x<100):
+        try:
+            gotdata = (ct['store'][x])
+            x=x+1
+        except IndexError:
+            gotdata = 'null'
+            maxy=x
+            print ('max = '+str(maxy))
+            break
+         
+    y=0
+    while (y<maxy):
+        un=ct['store'][y]['username']
+        print(str(un))
+        y=y+1
+    y=0
+    while (y<maxy):
+        tid=ct['store'][y]['tweet_id']
+        print(str(tid))
+        y=y+1
+    y=0
+    while (y<maxy):
+        sn=ct['store'][y]['screen_name']
+        print(str(sn))
+        y=y+1
+    y=0
+    while (y<maxy):
+        tt=ct['store'][y]['tweet_text']
+        print(str(tt))
+        y=y+1
+        
+    print('-----------------')
+         
+        
     contents.close()
+    
 processTweets()
